acfa916ba04516a9962c36323809e432e6816847
boboman13 <me@boboman13.net>
2014-02-03 09:57:55 -0800

    Adds a compiler script

--- make.sh
+++ make.sh
@@ -4,10 +4,14 @@ outputName="program.rcx"
 
 echo "Compiling via NQC automatically."
 
+# These are the flags we use.
+location="/dev/usb/legousbtower0"
+flags="-Lcompile.log -Ecompile.err -d -Trcx2 -S$location"
+
 # Start compiling.
 echo -n "Please enter the MAIN file: "
 read main
 
-nqc $main
+nqc $flags -O$outputName $main
 
 echo "Compiled $main to $outputName."
